d4674553d53b7fc0b7ed1dded82229800cc58236
Luiz Américo <luizamericop@gmail.com>
2018-06-27 09:46:30 -0300

    Do not copy package files to dist

--- tools/build.js
+++ tools/build.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const fs = require('fs')
 const del = require('del')
 const rollup = require('rollup')
 const babel = require('rollup-plugin-babel')
@@ -33,17 +32,6 @@ formats.forEach((format) => {
     sourcemap: true,
     name: format === 'umd' ? exportName : undefined
   })))
-})
-
-// Copy package.json and LICENSE.txt
-promise = promise.then(() => {
-  delete pkg.private
-  delete pkg.devDependencies
-  delete pkg.scripts
-  delete pkg.babel
-  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8')
-  fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8')
-  fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8')
 })
 
 promise.catch(err => console.error(err.stack)) // eslint-disable-line no-console
